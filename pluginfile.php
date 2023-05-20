e8b0d4c0a41757f566686dcc5f510ee93a5b79fc
Fabian Wagner <fabian@blax.at>
2023-05-20 15:11:34 +0200

    I structure

---
+++ pluginfile.php
@@ -0,0 +1,18 @@
+<?php
+
+// /*
+//  * Plugin Name: Blax Taxi Rostock CSV Fahrten
+//  * Description: CSV Importer
+//  * Version: 0.0.0
+//  * Author:      Blax Software & Consulting
+//  * Author URI:  https://www.blax.at
+//  * Text Domain: blax-taxirostock-csvfahrten
+//  * Requires at least: 5.x
+//  * Requires PHP: 7.4
+//  */
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+class Plugin
+{
+}
---
+++ setup.php
@@ -0,0 +1,15 @@
+<?php
+
+
+require __DIR__ . '/vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Sets up the project template to fit individual needs
+|--------------------------------------------------------------------------
+|
+*/
+
+new \Blax\Wordpress\Plugin();
+
+dd('Hi');
